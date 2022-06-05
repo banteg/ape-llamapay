a93435c8f19120163e2512c368909fb402d16336
banteg <banteeg@gmail.com>
2022-06-05 17:51:47 +0400

    feat: add deployment addresses

---
+++ ape_llamapay/constants.py
@@ -0,0 +1,30 @@
+FACTORY = {
+    "ethereum": {
+        "mainnet": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+        "rinkeby": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+        "kovan": "0xD43bB75Cc924e8475dFF2604b962f39089e4f842",
+    },
+    "optimism": {
+        "mainnet": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+    },
+    "arbitrum": {
+        "mainnet": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+    },
+    "avalanche": {
+        "mainnet": "0x7d507b4c2d7e54da5731f643506996da8525f4a3",
+        "fuji": "0xc4705f96030D347F421Fbe01d9A19F18B26a7d30",
+    },
+    "fantom": {
+        "opera": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+    },
+    "polygon": {
+        "mainnet": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+    },
+    "bsc": {
+        "mainnet": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+    },
+    # provisional name, there is no gnosis chain / xdai plugin yet
+    "gnosis": {
+        "mainnet": "0xde1C04855c2828431ba637675B6929A684f84C7F",
+    },
+}
