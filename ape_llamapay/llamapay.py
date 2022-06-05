3e62530bcc2d8ecbb2ab6db2434e28e23e6485d9
banteg <banteeg@gmail.com>
2022-06-05 22:37:11 +0400

    refactor: rename to pool

--- ape_llamapay/llamapay.py
+++ ape_llamapay/llamapay.py
@@ -24,17 +24,17 @@ class Factory(ManagerAccessMixin):
     def contract(self) -> ContractInstance:
         return ContractInstance(self.address, manifest.contract_types["LlamaPayFactory"])
 
-    def get_contract_for_token(self, token: str) -> "LlamaPay":
+    def get_pool_for_token(self, token: str) -> "LlamaPay":
         try:
             token = tokens[token].address
         except KeyError:
             pass
 
         address, is_deployed = self.contract.getLlamaPayContractByToken(token)
-        return LlamaPay(address, is_deployed)
+        return Pool(address, is_deployed)
 
 
-class LlamaPay(ManagerAccessMixin):
+class Pool(ManagerAccessMixin):
     def __init__(self, address: AddressType, is_deployed: bool):
         self.address = address
         self.is_deployed = is_deployed
@@ -48,4 +48,6 @@ class LlamaPay(ManagerAccessMixin):
         return ContractInstance(self.contract.token(), ERC20)
 
     def __repr__(self):
-        return f"<LlamaPay address={self.address} exists={self.is_deployed} token={self.token.symbol()}>"
+        return (
+            f"<Pool address={self.address} exists={self.is_deployed} token={self.token.symbol()}>"
+        )
