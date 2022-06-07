59c3ba9d13666709d257f69b7e3570a3a5d13721
banteg <banteeg@gmail.com>
2022-06-07 22:30:04 +0400

    test: add stream fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,6 @@ from ape_llamapay import llamapay
 
 
 @pytest.fixture(scope="session")
-def factory():
-    return llamapay.Factory()
-
-
-@pytest.fixture(scope="session")
-def pool():
-    return llamapay.Factory().get_pool("DAI")
-
-
-@pytest.fixture(scope="session")
 def ape(accounts):
     return accounts[0]
 
@@ -21,3 +11,18 @@ def ape(accounts):
 @pytest.fixture(scope="session")
 def babe(accounts):
     return accounts[1]
+
+
+@pytest.fixture(scope="session")
+def factory():
+    return llamapay.Factory()
+
+
+@pytest.fixture(scope="session")
+def pool(factory):
+    return factory.get_pool("DAI")
+
+
+@pytest.fixture(scope="session")
+def stream(pool):
+    return llamapay.Stream(source=ape, target=babe, rate=1e20, pool=pool)
